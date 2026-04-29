68956ac54aab8d28e2ce500f9894674f7dc3a66e
TereshonokMaksim2010 <121506340+TereshonokMaksim@users.noreply.github.com>
2026-04-29 17:49:39 +0200

    Schema for contacts
    Writing from phone - each file will have its own commit

--- src/modules/contact/contact.schema.ts
+++ src/modules/contact/contact.schema.ts
@@ -1,6 +1,12 @@
-// import * as yup from "yup";
+import * as yup from "yup";
 
-// export const createContactSchema = yup.object({
-//     localName: yup.string().required("Field is required"),
-//     contactUserId: yup.string().required("Field is required"),
-// })
+export const createContactSchema = yup.object({
+    localName: yup
+        .string()
+        .min(4, "Довжина назви контакту повинна бути вище 4 символів")
+        .max(255, "Довжина назви контакту повинна бути менша від 255 символів")
+        .required("Назва контакту обов'язкова"),
+    contactUserId: yup
+        .string()
+        .required("ID користувачу необхідний"),
+});
